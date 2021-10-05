fafca3fb739aa72c94ad0474453ab0874ba48e22
EC2 Default User <ec2-user@ip-192-168-108-202.us-east-2.compute.internal>
2021-10-05 19:04:57 +0000

    POST Method

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 import time
 import socket
 import requests
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 # cache = redis.Redis(host='redis.internal-bookingapp.com', port=6379)
@@ -12,10 +12,15 @@ def ingest_hl7():
     return message
 
 
-@app.route('/home')
-def hit():
-    count = ingest_hl7()
-    return "<html>HL7 Ingestion service on node %s.<br \>>Response : %s" % ( socket.gethostname(), count)
+@app.route('/hl7/<hl7msg>', methods = ['GET','POST'])
+def hit(hl7msg):
+    if request.method == 'GET':
+        count = ingest_hl7()
+        return "<html>HL7 Ingestion service on node %s.<br \>>Response : %s" % ( socket.gethostname(), count)
+    
+    if request.method == 'POST':
+        data = request.form
+        return "<html>HL7 Ingestion service on node %s.<br \>>Response : %s" % ( socket.gethostname(), data)
 
 
 if __name__ == "__main__":
