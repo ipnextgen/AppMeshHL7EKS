c4d42f1e237e492093304c5c28b4b73777036e76
EC2 Default User <ec2-user@ip-192-168-106-139.us-east-2.compute.internal>
2021-10-07 20:01:48 +0000

    Public ECR + RAM

--- src/hl7_ingest/build.sh
+++ src/hl7_ingest/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 docker build . -t hl7_ingest
-docker tag hl7_ingest 279855623390.dkr.ecr.us-east-2.amazonaws.com/hl7_ingest
-docker push 279855623390.dkr.ecr.us-east-2.amazonaws.com/hl7_ingest
+#docker tag hl7_ingest 279855623390.dkr.ecr.us-east-2.amazonaws.com/hl7_ingest
+#docker push 279855623390.dkr.ecr.us-east-2.amazonaws.com/hl7_ingest
+docker tag hl7_ingest public.ecr.aws/d9m0n6d3/hl7_ingest
+docker push public.ecr.aws/d9m0n6d3/hl7_ingest
